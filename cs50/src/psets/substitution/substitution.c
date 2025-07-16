ad82e90fd8f0cc5a07bf9bc30b894cf94b108ba9
David Yue <davidyue5819@gmail.com>
2025-07-16 15:54:40 -0700

    Update substitution pset to include key checking

--- cs50/src/psets/substitution/substitution.c
+++ cs50/src/psets/substitution/substitution.c
@@ -9,6 +9,35 @@ char substitution_encrypt (char c, char *key)
         return isupper (c) ? toupper (key[c - 65]) : (islower (c) ? tolower (key[c - 97]) : c);
 }
 
+void check_key (char *key)
+{
+        if (strlen (key) != 26) {
+                fprintf (stderr, "Invalid key");
+                exit(EXIT_FAILURE);
+        }
+
+        int letter_bitmap = 0;
+        for (char *c = key; *c != '\0'; c++) {
+                char curr = *c;
+                if (!isalpha (curr)) {
+                        fprintf (stderr, "Invalid key");
+                        exit (EXIT_FAILURE);
+                }
+
+                if (isupper (curr))
+                        curr -= 65;
+                else if (islower (curr))
+                        curr -= 97;
+
+                if (letter_bitmap & (1 << curr)) {
+                        fprintf (stderr, "Invalid key");
+                        exit (EXIT_FAILURE);
+                }
+
+                letter_bitmap |= (1 << curr);
+        }
+}
+
 int main (int argc, char **argv)
 {
         if (argc != 2) {
@@ -18,10 +47,7 @@ int main (int argc, char **argv)
 
         char *key = argv[1];
 
-        if (strlen (key) != 26) {
-                fprintf (stderr, "Key must contain 26 characters.\n");
-                exit (EXIT_FAILURE);
-        }
+        check_key(key);
 
         char *text = get_string ("plaintext:  ");
 
